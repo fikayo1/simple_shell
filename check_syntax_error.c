837bf4dcc49e9371c805a2acf2bda376174b2340
Michael <micfem2b@gmail>
2022-09-07 00:44:31 +0100

    checks for syntax errors

---
+++ check_syntax_error.c
@@ -0,0 +1,126 @@
+#include "main.h"
+
+/**
+ * repeated_char - counts the repetitions of a char
+ *
+ * @input: input string
+ * @i: index
+ * Return: repetitions
+ */
+int repeated_char(char *input, int i)
+{
+	if (*(input - 1) == *input)
+		return (repeated_char(input - 1, i + 1));
+
+	return (i);
+}
+
+/**
+ * error_sep_op - finds syntax errors
+ *
+ * @input: input string
+ * @i: index
+ * @last: last char read
+ * Return: index of error. 0 when there are no
+ * errors
+ */
+int error_sep_op(char *input, int i, char last)
+{
+	int count;
+
+	count = 0;
+	if (*input == '\0')
+		return (0);
+
+	if (*input == ' ' || *input == '\t')
+		return (error_sep_op(input + 1, i + 1, last));
+
+	if (*input == ';')
+		if (last == '|' || last == '&' || last == ';')
+			return (i);
+
+	if (*input == '|')
+	{
+		if (last == ';' || last == '&')
+			return (i);
+
+		if (last == '|')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	if (*input == '&')
+	{
+		if (last == ';' || last == '|')
+			return (i);
+
+		if (last == '&')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	return (error_sep_op(input + 1, i + 1, *input));
+}
+
+/**
+ * first_char - finds index of the first char
+ *
+ * @input: input string
+ * @i: index
+ * Return: 1 if there is an error. 0 in other case.
+ */
+int first_char(char *input, int *i)
+{
+
+	for (*i = 0; input[*i]; *i += 1)
+	{
+		if (input[*i] == ' ' || input[*i] == '\t')
+			continue;
+
+		if (input[*i] == ';' || input[*i] == '|' || input[*i] == '&')
+			return (-1);
+
+		break;
+	}
+
+	return (0);
+}
+
+/**
+ * print_syntax_error - prints when a syntax error is found
+ *
+ * @datash: data structure
+ * @input: input string
+ * @i: index of the error
+ * @bool: to control msg error
+ * Return: no return
+ */
+void print_syntax_error(data_shell *datash, char *input, int i, int bool)
+{
+	char *msg, *msg2, *msg3, *error, *counter;
+	int length;
+
+	if (input[i] == ';')
+	{
+		if (bool == 0)
+			msg = (input[i + 1] == ';' ? ";;" : ";");
+		else
+			msg = (input[i - 1] == ';' ? ";;" : ";");
+	}
+
+	if (input[i] == '|')
+		msg = (input[i + 1] == '|' ? "||" : "|");
+
+	if (input[i] == '&')
+		msg = (input[i + 1] == '&' ? "&&" : "&");
+
+	msg2 = ": Syntax error: \"";
+	msg3 = "\" unexpected\n";
+	counter = aux_itoa(datash->counter);
+
